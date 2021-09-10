93e42ef6d3cfdcde2229ef0a29b6fd5013a455c0
s.ofilan.445441@umindanao.edu.ph <s.ofilan.445441@umindanao.edu.ph>
2021-09-10 09:39:23 +0800

    update feature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 @section('after-styles')
 <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
 <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.2/Chart.min.js"></script>
 @endsection
 @section('content')
 <div class="container-fluid">
