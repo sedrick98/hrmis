f1de313b7a2a58a0650124cb7d20e4963b78b0bf
s.ofilan.445441@umindanao.edu.ph <s.ofilan.445441@umindanao.edu.ph>
2021-10-14 14:05:33 +0800

    updated login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,65 +2,73 @@
 
 @section('content')
 
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card-group">
-            <div class="card p-4">
-              <div class="card-body">
-                <h1>Login</h1>
-                <p class="text-muted">Sign In to your account</p>
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="input-group mb-3">
 
-                    @if (count($errors) > 0)
-                    <div class="error">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    @endif
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <svg class="c-icon">
-                          <use xlink:href="assets/icons/coreui/free-symbol-defs.svg#cui-user"></use>
-                        </svg>
-                      </span>
-                    </div>
-                    <input class="form-control" type="text" placeholder="{{ __('E-Mail Address') }}" name="email" value="{{ old('email') }}" required autofocus>
-                    </div>
-                    <div class="input-group mb-4">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <svg class="c-icon">
-                          <use xlink:href="assets/icons/coreui/free-symbol-defs.svg#cui-lock-locked"></use>
-                        </svg>
-                      </span>
-                    </div>
-                    <input class="form-control" type="password" placeholder="{{ __('Password') }}" name="password" required>
-                    </div>
-                    <div class="row">
-                    <div class="col-6">
-                        <button class="btn btn-primary px-4" type="submit">{{ __('Login') }}</button>
-                    </div>
-                    </form>
-                    <div class="col-6 text-right">
-                        <a href="route('password.request')" class="btn btn-link px-0">{{ __('Forgot Your Password?') }}</a>
-                    </div>
-                    </div>
+<body style="background-image: url('/images/logos/dost1.jpg'); background-size: cover">
+
+
+  <div class="container" style="width:750px; margin-top:-80px;">
+    <div>
+      <img src="{{asset('images/logos/dostlogo1.png')}}" alt="DOST 11 Logo" style="display:block; height:150px; width:150px; margin:auto">
+    </div><br>
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card-group">
+          <div class="card p-4">
+            <div class="card-body">
+              <h1>Login</h1>
+              <p class="text-muted">Sign In to your account</p>
+              <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="input-group mb-3">
+
+                  @if (count($errors) > 0)
+                  <div class="error">
+                    <ul>
+                      @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                      @endforeach
+                    </ul>
+                  </div>
+                  @endif
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <svg class="c-icon">
+                        <use xlink:href="assets/icons/coreui/free-symbol-defs.svg#cui-user"></use>
+                      </svg>
+                    </span>
+                  </div>
+                  <input class="form-control" type="text" placeholder="{{ __('E-Mail Address') }}" name="email" value="{{ old('email') }}" required autofocus>
+                </div>
+                <div class="input-group mb-4">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <svg class="c-icon">
+                        <use xlink:href="assets/icons/coreui/free-symbol-defs.svg#cui-lock-locked"></use>
+                      </svg>
+                    </span>
+                  </div>
+                  <input class="form-control" type="password" placeholder="{{ __('Password') }}" name="password" required>
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <button class="btn btn-primary px-4" type="submit">{{ __('Login') }}</button>
+                  </div>
+              </form>
+              <div class="col-6 text-right">
+                <a href="route('password.request')" class="btn btn-link px-0">{{ __('Forgot Your Password?') }}</a>
               </div>
-            </div>
-            <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
-              
             </div>
           </div>
         </div>
+
       </div>
     </div>
-    
+  </div>
+  </div>
+
+
+</body>
+
 
 @endsection
 
